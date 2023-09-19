79b3f3d9037832767dc319256425dabdf30b4513
zuqueto200 <ricardozuqueto@gmail.com>
2023-09-19 19:40:36 -0300

    teste data 190920231940

--- src/utils/formatedDays.js
+++ src/utils/formatedDays.js
@@ -1,11 +1,9 @@
 import moment from "moment";
+moment.locale("en"); // en
 
-export const currentDate = moment().format("YYYY/MM/DD HH:mm");
-export const nextday = moment(currentDate).add(1, "days").format("YYYY/MM/DD");
-
-export const today = moment(currentDate).format("dddd");
-export const tomorrow = moment(nextday).format("dddd");
-
-export const CurrentDatee = moment().format();
-
+export const currentDate = new Date().toISOString();
+export const nextday = moment(currentDate, "YYYY/MM/DD HH:mm").add(1, "days").format("YYYY/MM/DD");
+export const today = moment(currentDate, "YYYY/MM/DD HH:mm").format("dddd");
+export const tomorrow = moment(nextday, "YYYY/MM/DD").format("dddd");
+export const CurrentDatee = moment(new Date()).format();
 export const todayTime = moment(CurrentDatee).format("HH:mm");

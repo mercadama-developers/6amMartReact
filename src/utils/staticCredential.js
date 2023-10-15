44587451f4ec797aa730ff46f7c6de318b6cd34c
mercadama-developers <143348184+mercadama-developers@users.noreply.github.com>
2023-10-15 11:20:30 -0300

    Update staticCredential.js

--- src/utils/staticCredential.js
+++ src/utils/staticCredential.js
@@ -1,4 +1,4 @@
-export const google_client_id = "";
+export const google_client_id = "781814545449-pubhf9f49kuo1jkgg0dk2hchf2730nde.apps.googleusercontent.com";
 export const fb_app_id = "";
 export const appleLoginCredential = {
   appleId: "",
